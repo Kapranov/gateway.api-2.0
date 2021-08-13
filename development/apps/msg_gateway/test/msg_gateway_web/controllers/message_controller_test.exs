9848a11313df172a39c6f177b0e6345211cd2825
kapranov <o.kapranov@ehealth.gov.ua>
2021-08-13 09:22:01 +0000

    added MessageControllerTest

--- development/apps/msg_gateway/test/msg_gateway_web/controllers/message_controller_test.exs
+++ development/apps/msg_gateway/test/msg_gateway_web/controllers/message_controller_test.exs
@@ -2,19 +2,35 @@ defmodule MsgGatewayWeb.MessageControllerTest do
   use MsgGatewayWeb.ConnCase
   use Core.DataCase
 
-  test "create /message" do
+  test "create /message", %{conn: conn} do
+    response = create_message(conn, 200, ["data"])
+    assert get_in(response, ["body"])    == nil
+    assert get_in(response, ["contact"]) == nil
+    assert get_in(response, ["tag"])     == "111111111"
+  end
+
+  test "show /message/:id", %{conn: conn} do
+    created = create_message(conn, 200, ["data"])
+    message_id = get_in(created, ["message_id"])
+    response = show_message(conn, 200, message_id, ["data"])
+    assert get_in(response, ["message_id"])     ==  created["message_id"]
+    assert get_in(response, ["message_status"]) == "in_queue"
+  end
+
+  test "show /message/:id when not correct id", %{conn: conn} do
+    message_id = FlakeId.get()
+    response = show_message(conn, 200, message_id, ["data"])
+    assert get_in(response, ["message_id"])     == message_id
+    assert get_in(response, ["message_status"]) == "not_found"
+  end
+
+  test "change_message_status /sending/change_message_status" do
   end
 
   test "new_email /sending/email" do
   end
 
   test "queue_size /sending/queue_size" do
-  end
-
-  test "show /message/:id" do
-  end
-
-  test "change_message_status /sending/change_message_status" do
   end
 
 #  test "send message without auth", %{conn: conn} do
@@ -66,10 +82,36 @@ defmodule MsgGatewayWeb.MessageControllerTest do
 #    remove_key(get_in(key_couple, ["id"]), conn)
 #  end
 
+  defp create_message(conn, resp_status, attrs) do
+    post(conn, "/message", %{"resource" => %{
+      "body" => "Ваш код для підтвердження 451",
+      "contact" => "+3800000000000",
+      "tag" => "111111111"
+    }})
+    |> json_response(resp_status)
+    |> get_in(attrs)
+  end
+
+  defp show_message(conn, resp_status, message_id, attrs) do
+    get(conn, "/message/#{message_id}")
+    |> json_response(resp_status)
+    |> get_in(attrs)
+  end
+
 #  defp create_key(conn) do
 #    get(conn, "/api/keys")
 #    |> json_response(200)
 #    |> get_in(["data", "status"])
+#  end
+
+#  defp send_message(conn, resp_status, keys) do
+#    post(conn, "/message", %{"resource" => %{
+#      "tag" => "111111111",
+#      "contact" => "+3800000000000",
+#      "body" => "Ваш код для підтвердження 451"
+#    }})
+#    |> json_response(resp_status)
+#    |> get_in(keys)
 #  end
 
 #  defp select_all_keys(conn) do
@@ -86,16 +128,6 @@ defmodule MsgGatewayWeb.MessageControllerTest do
 
 #  defp add_auth_header(id, key, conn) do
 #    put_req_header(conn, "authorization", "Bearer "<> id <> ":" <> key)
-#  end
-
-#  defp send_message(conn, resp_status, keys) do
-#    post(conn, "/message", %{"resource" => %{
-#      "tag" => "111111111",
-#      "contact" => "+3800000000000",
-#      "body" => "Ваш код для підтвердження 451"
-#    }})
-#    |> json_response(resp_status)
-#    |> get_in(keys)
 #  end
 
 #  defp check_status(message_id, conn) do
