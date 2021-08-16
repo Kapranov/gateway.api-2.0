ea498fde6ddb568ddebfd17a002ac14470e636fb
kapranov <o.kapranov@ehealth.gov.ua>
2021-08-16 09:09:30 +0000

    added SmsRouter and SmsRouterTest

--- development/apps/sms_router/lib/sms_router.ex
+++ development/apps/sms_router/lib/sms_router.ex
@@ -1,3 +1,68 @@
 defmodule SmsRouter do
-  @moduledoc false
+  @moduledoc """
+  Documentation for SmsRouter.
+  """
+
+  alias Core.ContactsRequests
+  alias SmsRouter.RedisManager
+
+  @operator_codes :code
+  @messages_gateway_conf "system_config"
+  @operators_config "operators_config"
+
+  def check_and_send(message) do
+    ContactsRequests.get_by_phone_number!(message.contact)
+    |> check_operator_id(message)
+  end
+
+  defp check_operator_id(contact_info, message_info) when contact_info == nil, do: calc_operator_info(message_info)
+
+  defp check_operator_id(contact_info, message_info), do: calc_operator_info(message_info, contact_info.viber_id)
+
+  defp calc_operator_info(message_info, viber_id \\ nil ) do
+    phone_code = binary_part(message_info.contact, 0, 6)
+    RedisManager.get(@operators_config)
+    |> select_operators(phone_code)
+    |> select_protocol_for_send(message_info, viber_id)
+  end
+
+  defp select_operators({:error, _}, _), do: :default
+
+  defp select_operators([], _), do: :default
+
+  defp select_operators([%{configs: configs} = operator_info | other_operators], phone_code) do
+    case Map.has_key?(configs, @operator_codes) do
+      true ->  check_phone_belong_operator(operator_info, phone_code, other_operators)
+      _-> select_operators(other_operators, phone_code)
+     end
+  end
+
+  defp check_phone_belong_operator(%{configs: configs} = operator_info, phone_code, other_operators) do
+    case Enum.member?(String.split(configs.code), phone_code) do
+       true -> operator_info
+       _-> select_operators(other_operators, phone_code)
+    end
+  end
+
+  defp select_protocol_for_send(:default, message_info, _) do
+    mg_config = RedisManager.get(@messages_gateway_conf)
+
+    RedisManager.get(Map.get(mg_config, :default_sms_operator, "nil"))
+    |> send_to_protocol(message_info)
+  end
+
+  defp select_protocol_for_send(protocol_config, message_info, viber_id) do
+    ContactsRequests.add_operator_id(%{phone_number: message_info.contact, operator_id: protocol_config.id, viber_id: viber_id})
+    RedisManager.get(protocol_config.protocol_name)
+    |> send_to_protocol(message_info)
+  end
+
+  defp send_to_protocol({:error, _}, message_info)  do
+    system_config =  RedisManager.get(@messages_gateway_conf)
+    apply(String.to_atom(system_config.messages_router_module), String.to_atom(system_config.messages_router_method), [message_info])
+  end
+
+  defp send_to_protocol(protocol_config, message_info) do
+    apply(String.to_atom(protocol_config.module_name), String.to_atom(protocol_config.method_name), [message_info])
+  end
 end
--- development/apps/sms_router/test/sms_router_test.exs
+++ development/apps/sms_router/test/sms_router_test.exs
@@ -1,3 +1,105 @@
 defmodule SmsRouterTest do
   use ExUnit.Case
+  use Core.DataCase
+
+  alias SmsRouter.RedisManager
+
+  doctest SmsRouter
+
+  @sys_config %{
+    automatic_prioritization: false,
+    default_sms_operator: "",
+    messages_router_method: "send",
+    messages_router_module: __MODULE__,
+    org_name: "test",
+    sending_time: "60"
+  }
+
+  @sms_protocol_1 %{
+    active: true,
+    active_protocol_type: true,
+    configs: %{
+      code: "+38067",
+      sms_price_for_external_operator: 11
+    },
+    id: nil,
+    limit: 1000,
+    method_name: "first_sms_protocol",
+    module_name: __MODULE__,
+    operator_priority: 1,
+    priority: 1,
+    protocol_name: "first_sms_protocol"
+  }
+
+  @sms_protocol_2 %{
+    active: true,
+    active_protocol_type: true,
+    configs: %{
+      code: "+38000",
+      sms_price_for_external_operator: 11
+    },
+    id: nil,
+    limit: 1000,
+    method_name: "second_sms_protocol",
+    module_name: __MODULE__,
+    operator_priority: 1,
+    priority: 1,
+    protocol_name: "second_sms_protocol"
+  }
+
+  @sms_info %{
+    active: true,
+    body: "test",
+    contact: "+380000000000",
+    message_id: "a6ebd966-11a5-4f50-1111-9fc00a3b8469",
+    priority_list: [@sms_protocol_1, @sms_protocol_2],
+    sending_status: "sending"
+  }
+
+  @operators_config [@sms_protocol_1, @sms_protocol_2]
+
+  test "app start" do
+    SmsRouter.Application.start(nil,nil)
+  end
+
+  test "sms_router" do
+    message_id = "test_id678"
+    RedisManager.set("system_config", @sys_config)
+    RedisManager.set(message_id, %{
+      active: true,
+      body: "test message",
+      callback_url: "",
+      contact: "+380500000000",
+      message_id: message_id,
+      priority_list: [],
+      sending_status: "in_queue"
+    })
+
+    assert :ok == SmsRouter.check_and_send(%{contact: "+380500000000", body: "test message", message_id: message_id})
+    assert {:ok, 1} == RedisManager.del("system_config")
+
+    RedisManager.keys("system_config")
+    RedisManager.del("system_config1")
+
+    assert {:ok, 1} == RedisManager.del(message_id)
+  end
+
+  test "select operators sms_router" do
+    RedisManager.set("system_config", @sys_config)
+    RedisManager.set(get_in(@sms_protocol_1, [:protocol_name]), @sms_protocol_1)
+    RedisManager.set(get_in(@sms_protocol_2, [:protocol_name]), @sms_protocol_2)
+    RedisManager.set("operators_config", @operators_config)
+    RedisManager.set(get_in(@sms_info, [:message_id]), @sms_info)
+
+    assert :second_sms_protocol == SmsRouter.check_and_send(@sms_info)
+    assert {:ok, 1} == RedisManager.del("system_config")
+    assert {:ok, 1} == RedisManager.del("operators_config")
+    assert {:ok, 1} == RedisManager.del(get_in(@sms_protocol_1, [:protocol_name]))
+    assert {:ok, 1} == RedisManager.del(get_in(@sms_protocol_2, [:protocol_name]))
+    assert {:ok, 1} == RedisManager.del(get_in(@sms_info, [:message_id]))
+  end
+
+  def send(_), do: :ok
+  def first_sms_protocol(_), do: :first_sms_protocol
+  def second_sms_protocol(_), do: :second_sms_protocol
 end
