30ee7814a4b561e40a4a2778b65627160b71c804
kapranov <o.kapranov@ehealth.gov.ua>
2021-08-16 08:29:47 +0000

    fixed SmsRouter.Application

--- development/apps/sms_router/lib/sms_router/application.ex
+++ development/apps/sms_router/lib/sms_router/application.ex
@@ -4,8 +4,28 @@ defmodule SmsRouter.Application do
   use Application
 
   @impl true
+  @spec start(:normal | {:takeover, atom()} | {:failover, atom()}, start_args :: term()) ::
+          {:ok, pid()} | {:ok, pid(), term()} | {:error, reason :: term()}
   def start(_type, _args) do
-    children = []
+    config = Application.get_env(:sms_router,  SmsRouter.RedisManager)
+    hostname = config[:host]
+    password = config[:password]
+    database = config[:database]
+    port = config[:port]
+    pool_size =  String.to_integer(config[:pool_size])
+    children =
+      for i <- 0..(pool_size - 1) do
+        Supervisor.child_spec(
+          {
+            Redix, {
+              "redis://#{password}@#{hostname}:#{port}/#{database}",
+              name: :"redis_#{i}"
+            }
+          },
+          id: {Redix, i}
+        )
+      end
+
     opts = [strategy: :one_for_one, name: SmsRouter.Supervisor]
     Supervisor.start_link(children, opts)
   end
